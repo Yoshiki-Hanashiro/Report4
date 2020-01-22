5b894749acff443a59878601c5390403d82a44f4
Yoshiki Hanashiro <e195733@ie.u-ryukyu.ac.jp>
2020-01-22 13:07:20 +0900

    Integer.parseIntを使用して例外を発生させたコード

--- src/main/Report4.java
+++ src/main/Report4.java
@@ -1,14 +1,6 @@
 public class Report4 {
-    public static void main(String[] args){
-        try {
-            String str = null;
-            if(str == null){
-                throw new NullPointerException("値がnullになっています。");
-            }
-            System.out.println(str.length());
-        }catch (NullPointerException e){
-            System.out.println("例外が発生しました。");
-            System.out.println(e.getMessage());
-        }
+    public static void main(String[] args) {
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
